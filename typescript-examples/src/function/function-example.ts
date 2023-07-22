bbd4e0a9e78db3daec61cbc0e21969f05a066a81
omid mohebbi <omidmohebbise@gmail.com>
2023-07-22 16:03:43 +0330

    add typescript example project with afew codes

---
+++ typescript-examples/src/function/function-example.ts
@@ -0,0 +1,10 @@
+function voidFunc () : void{
+    console.log("I will not return anything :) ")
+}
+
+function returnFullName():string{
+    return "Omid Mohebbi"
+}
+voidFunc();
+console.log("return value : ", returnFullName() );
+
